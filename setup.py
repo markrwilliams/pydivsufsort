780ad172022d42d34166b87c08e5560c1cad2558
Mark Williams <markrwilliams@gmail.com>
2013-11-19 23:36:47 -0800

    setup.py fixes: static compilation and cython build_ext

--- setup.py
+++ setup.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
-from glob import glob
 import subprocess
 from os.path import exists
 
@@ -9,26 +7,28 @@ from distutils.core import setup
 from distutils.extension import Extension
 from distutils.command.build_py import build_py as _build_py
 
-from Cython.Build import cythonize
+from Cython.Distutils import build_ext
 
 
 class build_py(_build_py):
     if exists('lib/Makefile'):
         make_clean = subprocess.Popen(['make', 'distclean'], cwd='lib/')
         make_clean.wait()
-    configure = subprocess.Popen(['./configure', '--enable-shared'],
+    configure = subprocess.Popen(['./configure', '--with-pic'],
                                  cwd='lib/')
     configure.wait()
     make = subprocess.Popen(['make', '-j'], cwd='lib/')
     make.wait()
 
 setup(
-    cmdclass={'build_py': build_py},
+    cmdclass={'build_py': build_py,
+              'build_ext': build_ext},
     name='divsufsort',
-    include_dirs=['lib'],
     ext_modules=[
         Extension(
             "suffixarray",
-            sources=glob('src/*.c'),
+            sources=['src/suffix.pyx'],
+            libraries=['lib/lib/.libs/libdivsufsort.a'],
+            extra_objects=['lib/lib/.libs/libdivsufsort.a'],
             include_dirs=['lib/include'],
             language="c")])
--- src/suffix.pyx
+++ src/suffix.pyx
@@ -1,9 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
 
 cimport divsufsort
-
 
 cdef class SuffixArray:
     def __init__(self):
